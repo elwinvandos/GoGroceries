291ff0a57d5191e30ead08ca79ab20c9d38f0670
Elwin Van Dosselaer <e.van.dosselaer@gmail.com>
2023-05-08 15:06:43 +0200

    feat: migrate db in prod

--- Elwin.GoGroceries/Server/Program.cs
+++ Elwin.GoGroceries/Server/Program.cs
@@ -1,6 +1,9 @@
 using Elwin.GoGroceries.API.Configurations;
 using Elwin.GoGroceries.Core.Extensions;
+using Elwin.GoGroceries.Infrastructure.DbContexts;
 using Elwin.GoGroceries.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,12 +25,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+
+    using (var context = app.Services.GetService<GroceriesContext>())
+        context.Database.Migrate();
 }
 else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsProduction())
+{
+    using (var context = app.Services.GetService<GroceriesContext>())
+        context.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
