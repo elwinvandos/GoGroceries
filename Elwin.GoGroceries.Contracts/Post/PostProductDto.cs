4e6fec42b052d3b69e4f64e09ef7ee9457fb9997
Elwin Van Dosselaer <e.van.dosselaer@gmail.com>
2025-02-17 14:18:41 +0100

    Fix faulty template or product check

--- Elwin.GoGroceries.Contracts/Post/PostProductDto.cs
+++ Elwin.GoGroceries.Contracts/Post/PostProductDto.cs
@@ -4,7 +4,8 @@ namespace Elwin.GoGroceries.Contracts.Post
 {
     public record PostProductDto : NamedDtoBase
     {
-        public CategoryDto Category { get; set; } = new();
+        // This used to be not nullable and constructed a new object, I removed it now keep an eye on this
+        public CategoryDto? Category { get; set; }
         public int? Quantity { get; set; }
         public string? Measurement { get; set; }
         public int? MeasurementQuantity { get; set; }
