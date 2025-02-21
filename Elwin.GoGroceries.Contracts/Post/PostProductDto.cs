813534304b21517b4473a70ba5af00ead49c0057
Elwin Van Dosselaer <e.van.dosselaer@gmail.com>
2025-02-21 12:17:43 +0100

    Fix new category component and implement clientside error handling

--- Elwin.GoGroceries.Contracts/Post/PostProductDto.cs
+++ Elwin.GoGroceries.Contracts/Post/PostProductDto.cs
@@ -4,8 +4,7 @@ namespace Elwin.GoGroceries.Contracts.Post
 {
     public record PostProductDto : NamedDtoBase
     {
-        // This used to be not nullable and constructed a new object, I removed it now keep an eye on this
-        public CategoryDto? Category { get; set; }
+        public CategoryDto Category { get; set; } = new();
         public int? Quantity { get; set; }
         public string? Measurement { get; set; }
         public int? MeasurementQuantity { get; set; }
